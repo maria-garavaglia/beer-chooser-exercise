d58ce670e465173802e35299c74c817904ccdad1
Maria Garavaglia <maria.renie.garavaglia@gmail.com>
2025-04-29 12:47:59 -0400

    Start implementing Spring Boot back end, json files now properly loading

--- src/main/java/com/dearmariarenie/Main.java
+++ src/main/java/com/dearmariarenie/Main.java
@@ -4,48 +4,82 @@ import com.dearmariarenie.models.Beer;
 import com.dearmariarenie.models.Brewery;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@SpringBootApplication
 public class Main
 {
-    private static final String BEERS_FILENAME = "beers.json";
-    private static final String BREWERIES_FILENAME = "breweries.json";
 
     public static void main(String[] args)
     {
-        var app = new Main();
+        SpringApplication.run(Main.class, args);
+    }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer()
+        {
+            @Override
+            public void addCorsMappings(CorsRegistry registry)
+            {
+                registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("*")
+                    .allowedHeaders("*")
+                    .allowCredentials(false).maxAge(3600)
+                ;
+            }
+        };
+    }
+
+    @Bean
+    public List<Beer> beerList(@Value("classpath:beers.json") Resource data)
+    {
+        final String BEERS_FILENAME = "beers.json";
         var jsonReader = new ObjectMapper();
-        List<Beer> beerList = null;
-        List<Brewery> breweryList = null;
-
         try
         {
             System.out.print("Loading beers from JSON... ");
-            beerList = jsonReader.readValue(Main.class.getResource(BEERS_FILENAME), new TypeReference<List<Beer>>(){});
+            var beerList = jsonReader.readValue(
+                data.getFile(),
+                new TypeReference<List<Beer>>(){}
+            );
             System.out.println(beerList.size() + " beers loaded");
+            return beerList;
         }
         catch (IOException e)
         {
             System.out.println("Failed to load beer list '" + BEERS_FILENAME + "': " + e.getMessage());
-            beerList = new ArrayList<>();
+            return new ArrayList<>();
         }
+    }
 
+    @Bean
+    public List<Brewery> breweryList(@Value("classpath:breweries.json") Resource data)
+    {
+        final String BREWERIES_FILENAME = "breweries.json";
+        var jsonReader = new ObjectMapper();
         try
         {
             System.out.print("Loading breweries from JSON... ");
-            breweryList = jsonReader.readValue(Main.class.getResource(BREWERIES_FILENAME), new TypeReference<List<Brewery>>(){});
+            var breweryList = jsonReader.readValue(data.getFile(), new TypeReference<List<Brewery>>(){});
             System.out.println(breweryList.size() + " beers loaded");
+            return breweryList;
         }
         catch (IOException e)
         {
             System.out.println("Failed to load brewery list '" + BREWERIES_FILENAME + "': " + e.getMessage());
-            breweryList = new ArrayList<>();
+            return new ArrayList<>();
         }
     }
 }
