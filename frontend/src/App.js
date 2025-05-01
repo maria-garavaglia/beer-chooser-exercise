d5520383288a7dab3bde2a358d96a7dd6c367316
Maria Garavaglia <maria.renie.garavaglia@gmail.com>
2025-05-01 15:08:57 -0400

    Fleshes out filter criteria UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,38 @@ import './App.css';
 
 export default function App()
 {
+    function applyFilter()
+    {
+        console.log("Applying filter");
+    }
+
     return (
-        <>
-            <label>
-                Search:
-                <input name="searchTerm" />
-            </label>
-        </>
+        <div>
+            <FilterForm action={applyFilter} />
+        </div>
     );
+}
+
+function FilterForm({action})
+{
+    return (
+        <form action={action}>
+            <div>
+                Name:
+                <input name="nameInput" />
+            </div>
+            <div>
+                Style:
+                <input name="styleInput" />
+            </div>
+            <div>
+                ABV:
+                <input name="abvMinInput" />
+                -
+                <input name="abvMaxInput" />
+            </div>
+            <button type="submit">Apply</button>
+        </form>
+    );
+
 }
