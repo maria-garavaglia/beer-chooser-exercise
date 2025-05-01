67347b6ed79dcb6d53de0cf7fcf98e3b2c6ab90b
Maria Garavaglia <maria.renie.garavaglia@gmail.com>
2025-05-01 18:10:06 -0400

    Filter creation works, just need to pull from back end now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,51 +12,103 @@ export default function App()
             style: "Test Beer Style",
             description: "A beer used to test the Beer Chooser",
             brewery: 1234
+        },
+        {
+            id: 1,
+            name: "Test Beer 2",
+            abv: 5.0,
+            style: "Test Beer Style 2",
+            description: "Another beer used to test the Beer Chooser",
+            brewery: 5678
         }
     ]);
 
-    function applyFilter()
-    {
-        console.log("Applying filter");
-    }
-
     return (
         <div>
-            <FilterForm action={applyFilter} />
+            <FilterForm />
             <BeerListView beerList={beerList} />
         </div>
     );
 }
 
-function FilterForm({action, beerList})
+function FilterForm()
 {
+    const [filter, setFilter] = useState({
+        name: "",
+        style: "",
+        abvMin: 0.0,
+        abvMax: 100.0
+    });
+
+    function handleChangeName(e)
+    {
+        setFilter({
+            ...filter,
+            name: e.target.value
+        });
+    }
+
+    function handleChangeStyle(e)
+    {
+        setFilter({
+            ...filter,
+            style: e.target.value
+        });
+    }
+
+    function handleChangeAbvMin(e)
+    {
+        // TODO error checking
+        setFilter({
+            ...filter,
+            abvMin: e.target.value
+        });
+    }
+
+    function handleChangeAbvMax(e)
+    {
+        // TODO error checking
+        setFilter({
+            ...filter,
+            abvMax: e.target.value
+        });
+    }
+
+    function applyFilter(e)
+    {
+        // TODO query beer database
+        console.log("Applying filter");
+        console.log(filter);
+    }
+
     return (
-        <form action={action}>
+        <div>
             <div>
                 Name:
-                <input name="nameInput" />
+                <input value={filter.name} onChange={handleChangeName} />
             </div>
             <div>
                 Style:
-                <input name="styleInput" />
+                <input value={filter.style} onChange={handleChangeStyle} />
             </div>
             <div>
                 ABV:
-                <input name="abvMinInput" />
+                <input value={filter.abvMin} onChange={handleChangeAbvMin} />
                 -
-                <input name="abvMaxInput" />
+                <input value={filter.abvMax} onChange={handleChangeAbvMax} />
             </div>
-            <button type="submit">Apply</button>
-        </form>
+            <button onClick={applyFilter}>Apply</button>
+        </div>
     );
 }
 
 function BeerListView({beerList})
 {
     const beerViews = beerList.map(beer =>
-        <div>
+        <div key={beer.id}>
             <p>
-                <h3>{beer.name}</h3>
+                {beer.name}
+                <br />
                 {beer.abv}% - {beer.brewery}
                 <br />
                 {beer.style}
