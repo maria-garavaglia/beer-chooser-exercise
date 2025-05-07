0d41c40395b1b733d2d10959d58d8f54a714b2f3
Maria Garavaglia <maria.renie.garavaglia@gmail.com>
2025-05-07 16:07:08 -0400

    Better formatting of brewery description

--- frontend/src/BeerView.js
+++ frontend/src/BeerView.js
@@ -2,6 +2,31 @@ import './BeerView.css'
 
 export default function BeerListView({beer})
 {
+    function formatBrewery()
+    {
+        if (beer.brewery)
+        {
+            var brewery = beer.brewery;
+
+            var str = brewery.name;
+            if (brewery.city)
+            {
+                str += " - " + brewery.city;
+                if (brewery.state)
+                {
+                    str += ", " + brewery.state;
+                }
+            }
+            else if (brewery.state)
+            {
+                str += " - " + beer.brewery.state;
+            }
+            return str;
+        }
+
+        return "Brewery Unknown";
+    }
+
     return (
         <div class="beerView">
             <h3 class="name">
@@ -13,13 +38,7 @@ export default function BeerListView({beer})
             <p class="description">
                 {beer.description}
             </p>
-            <p class="brewery">{
-                beer.brewery
-                    ? beer.brewery.name + " - "
-                        + beer.brewery.city + ", "
-                        + beer.brewery.state
-                    : "Unknown"
-            }</p>
+            <p class="brewery">{formatBrewery()}</p>
         </div>
     );
 }
