d3bc9e544e1c5a070affa30a91223920aebdb7fa
Maria Garavaglia <maria.renie.garavaglia@gmail.com>
2025-05-07 16:48:00 -0400

    Addresses BeerService TODO

--- backend/src/main/java/com/dearmariarenie/beerchooser/beers/Beer.java
+++ backend/src/main/java/com/dearmariarenie/beerchooser/beers/Beer.java
@@ -13,4 +13,34 @@ public record Beer(
     int upc,
     String style_name,
     String descript
-) {}
+) {
+    /**
+     * Checks if this beer matches a particular set of search criteria
+     */
+    public boolean matchesSearch(BeerSearchCriteria criteria)
+    {
+        // If criterion is empty, use a default value that will catch everything
+        return
+            containsIgnoreCase(name, criteria.name().orElse(""))
+            && containsIgnoreCase(style_name, criteria.style().orElse(""))
+            && abv >= criteria.abvMin().orElse(0.0)
+            && abv <= criteria.abvMax().orElse(100.0)
+        ;
+    }
+
+    /**
+     * convenience function for finding substrings while ignoring case
+     */
+    private boolean containsIgnoreCase(String str, String sub)
+    {
+        if (str == null)
+        {
+            return sub == null || sub.isEmpty();
+        }
+        if (sub == null)
+        {
+            return true;
+        }
+        return str.toLowerCase().contains(sub.toLowerCase());
+    }
+}
--- backend/src/main/java/com/dearmariarenie/beerchooser/beers/BeerService.java
+++ backend/src/main/java/com/dearmariarenie/beerchooser/beers/BeerService.java
@@ -26,38 +26,18 @@ public class BeerService
 
     public List<BeerSummary> searchBeers(BeerSearchCriteria criteria)
     {
-        // Filter out beers by each criterion. If criterion is empty, use a default value that will
-        // catch everything
-        // TODO maybe make this a function in Beer class instead?
+        // Filter out beers by each criterion, then convert each to a summary class containing data to be displayed
         return beerList.stream()
-            .filter(b -> containsIgnoreCase(b.name(), criteria.name().orElse("")))
-            .filter(b -> containsIgnoreCase(b.style_name(), criteria.style().orElse("")))
-            .filter(b -> b.abv() >= criteria.abvMin().orElse(0.0))
-            .filter(b -> b.abv() <= criteria.abvMax().orElse(100.0))
+            .filter(b -> b.matchesSearch(criteria))
             .map(beer ->
                 new BeerSummary(
                     beer,
+                    // find brewery corresponding to ID (if it exists)
                     findBrewery(beer.brewery_id()).orElse(null)
                 )
             )
             .toList()
         ;
-    }
-
-    /**
-     * convenience function for finding substrings while ignoring case
-     */
-    private boolean containsIgnoreCase(String str, String sub)
-    {
-        if (str == null)
-        {
-            return sub == null || sub.isEmpty();
-        }
-        if (sub == null)
-        {
-            return true;
-        }
-        return str.toLowerCase().contains(sub.toLowerCase());
     }
 
     private Optional<Brewery> findBrewery(int breweryId)
